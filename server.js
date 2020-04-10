5f61977cbc08996db1f75d367dabe02e38cb9c75
iamgeorge <george.eyoyibo@gmail.com>
2020-04-11 02:32:56 +0300

    contact model & contact route

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => res.send({ msg: 'Welcome to the Contact App' }));
 const PORT = process.env.PORT || 9000;
 
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contact', require('./routes/contact'));
+app.use('/api/contacts', require('./routes/contact'));
 app.use('/api/auth', require('./routes/auth'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
