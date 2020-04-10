31c1cf6723bcac81989dfffb4768b8820f43a9c5
iamgeorge <george.eyoyibo@gmail.com>
2020-04-10 21:43:11 +0300

    user model & Validation

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
@@ -10,7 +10,7 @@ const User = require('../models/User');
 router.post(
   '/',
   [
-    check('name', 'Please')
+    check('name', 'Please add name')
       .not()
       .isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
@@ -20,7 +20,12 @@ router.post(
     ).isLength({ min: 6 })
   ],
   (req, res) => {
-    res.send(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    res.send('passed');
   }
 );
 
