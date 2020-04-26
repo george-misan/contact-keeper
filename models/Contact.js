60a36169b6c9d99d08d084fabe6c56cda95e7375
iamgeorge <george.eyoyibo@gmail.com>
2020-04-26 20:36:15 +0300

    create Contact Update route logic

--- models/Contact.js
+++ models/Contact.js
@@ -24,10 +24,6 @@ const ContactSchema = mongoose.Schema({
     type: Date,
     default: Date.now,
   },
-  password: {
-    type: String,
-    required: true,
-  },
 });
 
 module.exports = mongoose.model('contact', ContactSchema);
--- routes/contact.js
+++ routes/contact.js
@@ -31,6 +31,8 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const { name, email, phone, type } = req.body;
     try {
       const newContact = new Contact({
         name,
@@ -43,7 +45,7 @@ router.post(
       const contact = await newContact.save(); //save in db
 
       res.json(contact);
-    } catch (error) {
+    } catch (err) {
       console.error(err.message);
       res.status(400).send('Server Error');
     }
@@ -52,8 +54,37 @@ router.post(
 //@route        PUT api/contact/:id
 //@desc         Update contacts
 //@access       Private
-router.get('/:id', (req, res) => {
-  res.send('Update contacts');
+router.get('/:id', auth, async (req, res) => {
+  const { name, email, phone, type } = req.body;
+
+  // Build contact object
+  const contactFields = {};
+  if (name) contactFields.name = name;
+  if (email) contactFields.email = email;
+  if (phone) contactFields.phone = phone;
+  if (type) contactFields.type = type;
+
+  try {
+    let contact = await Contact.findById(req.params.id);
+
+    if (!contact) return res.status(404).json({ msg: 'Contact not found' });
+
+    // Make sure user owns contact
+    if (contact.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Not authorized' });
+    }
+
+    contact = await Contact.findByIdAndUpdate(
+      req.params.id,
+      { $set: contactFields },
+      { new: true }
+    );
+
+    res.json(contact);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 //@route        DELETE api/contact/:id
 //@desc         Delete contacts
