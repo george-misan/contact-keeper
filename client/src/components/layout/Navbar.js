adf32e9f5054ed4ff7de972600b7db28f25a96ab
iamgeorge <george.eyoyibo@gmail.com>
2020-04-27 19:58:01 +0300

    add pages and Router

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 const Navbar = ({ title, icon }) => {
   return (
@@ -6,6 +8,14 @@ const Navbar = ({ title, icon }) => {
       <h1>
         <i className={icon} /> {title}
       </h1>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+      </ul>
     </div>
   );
 };
@@ -17,7 +27,7 @@ Navbar.propTypes = {
 
 Navbar.defaultProps = {
   title: 'Contact Keeper',
-  icon: '',
+  icon: 'fas fa-id-card-alt',
 };
 
 export default Navbar;
